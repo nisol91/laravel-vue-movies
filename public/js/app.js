733edbb0200fc3a45b46ec3992e02f9570aa3c30
nisol91 <nicolasolzi91@gmail.com>
2019-03-12 15:32:41 +0100

    creato vue component ed esempio statico con array di oggetti movie

--- public/js/app.js
+++ public/js/app.js
@@ -49736,6 +49736,19 @@ var app = new Vue({
       content: 'gvv',
       img: 'http'
     }]
+  },
+  mounted: function mounted() {
+    var axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js"); // Make a request for a user with a given ID
+
+
+    axios.get('/user?ID=12345').then(function (response) {
+      // handle success
+      console.log(response);
+    }).catch(function (error) {
+      // handle error
+      console.log(error);
+    }).then(function () {// always executed
+    });
   }
 });
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -35,6 +35,23 @@ const app = new Vue({
             { title: 'Adw', year: '1950', content: 'vdg', img: 'http' },
             { title: 'Avcc', year: '1997', content: 'gvv', img: 'http' },
        ]
+    },
+    mounted() {
+            const axios = require('axios');
+
+            // Make a request for a user with a given ID
+            axios.get('/user?ID=12345')
+                .then(function (response) {
+                    // handle success
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .then(function () {
+                    // always executed
+                });
     }
 });
 
